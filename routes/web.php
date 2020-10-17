53b7094e19ea506e34b32b53905785d96651a932
hhyzhanglei <605060306@qq.com>
2020-10-17 16:40:28 +0800

    分类话题

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,4 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 // Route::any('/users/{user}', 'UsersController@update')->name('users.update');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
